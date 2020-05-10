bb769a6d4b5619ce4da4a0fa3af4745d1f77335d
SuperCode <supercode@SuperCodes-MacBook-Air.local>
2020-05-10 15:11:34 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+function weather (quality){
+  if ((quality > 8)&&(quality<10)){
+    let a ="super" 
+    return a
+
+  }  
+  else if ((quality > 6)&&(quality<8)){
+   let b= "gut"
+    return b    
+  }  
+  else if ((quality > 3)&&(quality<6)){
+    return "okay"    
+  }  
+  else if ((quality > 0)&&(quality<3)){
+    return "schlecht" 
+  }  
+
+}
+
+
